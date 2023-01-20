fc27c1717aa99c7dd18782756441b9cceae9b8c1
jihhyeon <sjh1031201@naver.com>
2023-01-20 16:52:12 +0900

    2805_백준

--- Binary search/2805.py
+++ Binary search/2805.py
@@ -1,9 +1,11 @@
 n, m = map(int,input().split())
 trees = list(map(int, input().split()))
-start, end = 1, sum(trees)
+start, end = 1, max(trees)
+print(start, end)
 
 while start <= end:
     mid = (start+end)//2
+    print('mid: ', mid)
 
     #벌목된 나무 총합
     log = 0
@@ -12,8 +14,11 @@ while start <= end:
             log += i-mid
     print('log: ', log)
 
-    if log >= m:
+    if log >= m:#벌목된 나무 총합이 중간보다 크면
         start = mid+1
-    else:
+        #위로가서 바뀐 start값으로 mid 재설정
+
+    else:#벌목된 나무 총합이 중간보다 작으면
         end = mid-1
-print(end)
+        #위로가서 바뀐 end값으로 mid 재설정
+print(end)#높이의 최대값이기 때문에 end 출력

4cada1368d9b3ca336113caef16f705b6a33c350
jihhyeon <sjh1031201@naver.com>
2023-03-16 15:44:55 +0900

    save

--- Dynamic programming/test2.py
+++ Dynamic programming/test2.py
@@ -1,3 +1,4 @@
+#i-1, i-2만 보면 된다
 n= int(input())#식량 창고 개수(3<=n<=100)
 cnt = list(map(int, input().split()))#각 창고에 저장된 식량의 개수 k
 
@@ -8,5 +9,6 @@ d[1] = max(cnt[0],cnt[1])
 
 for i in range(2,n):
   d[i] = max(d[i-1], d[i-2]+cnt[i])
+  print(i, d[i])
 
 print(cnt[n-1])

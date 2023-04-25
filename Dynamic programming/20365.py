7cd4952f2b3852d5e92c196fef49ba889642f21a
jihhyeon <sjh1031201@naver.com>
2023-04-25 15:46:07 +0900

    save

--- Dynamic programming/20365.py
+++ Dynamic programming/20365.py
@@ -1,7 +1,7 @@
 n = int(input())
 array = list(map(str, input().rstrip()))
 color = {'R' : 0, 'B' : 0}
-color[array[0]] += 1
+color[array[0]] += 1#시작부분은 1 더해주기
 for i in range(1, n):
     if array[i] != array[i-1]:
 

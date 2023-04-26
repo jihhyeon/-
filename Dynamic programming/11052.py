f91698e330cf01a0504e43e39b28a81be77acada
jihhyeon <sjh1031201@naver.com>
2023-04-26 20:05:27 +0900

    save

--- Dynamic programming/11052.py
+++ Dynamic programming/11052.py
@@ -15,8 +15,7 @@ for i in range(n):
 print(max(dp))"""
 n = int(input())
 d = [0] * (n+1)
-p = [0] + list(map(int, input().split()))
-print(p)
+p = [0] + list(map(int, input().split()))#리스트 이어서 붙이기
 d[1] = p[1]
 for i in range(2, n+1):
     for j in range(1, i+1):

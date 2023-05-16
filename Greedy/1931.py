5445b5e252a6d12169e42c33e7c9d29b2275177c
jihhyeon <sjh1031201@naver.com>
2023-05-16 15:52:31 +0900

    save

---
+++ Greedy/1931.py
@@ -0,0 +1,12 @@
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+#이 문제의 포인트!
+graph.sort(key = lambda x: (x[1], x[0]))#끝나는 시각, 시작 시각 순서로 오름차순 정렬
+
+cnt = 1
+end_time = graph[0][1]
+for i in range(1, n):
+    if graph[i][0] >= end_time:
+        cnt += 1
+        end_time = graph[i][1]
+print(cnt)

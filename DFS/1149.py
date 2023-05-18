349a33060e3e22566f4bdcbf51a30e0853d0f2a2
jihhyeon <sjh1031201@naver.com>
2023-05-18 10:16:04 +0900

    save

--- DFS/1149.py
+++ DFS/1149.py
@@ -1,6 +1,7 @@
 n = int(input())
 array = [list(map(int, input().split())) for _ in range(n)]
 
+#이전의 값들 중 같은 색을 제외한 min값 더해주기
 for i in range(1,n):
     array[i][0] = min(array[i-1][1], array[i-1][2]) + array[i][0]
     array[i][1] = min(array[i-1][0], array[i-1][2]) + array[i][1]

3b295a6349a807163cb0da817e781d4ce72bf916
jihhyeon <sjh1031201@naver.com>
2023-06-21 19:50:56 +0900

    save

---
+++ 구현/메뉴 리뉴얼.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    
+    for c in course:
+        temp = []
+        
+        for order in orders:
+            combi = combinations(sorted(order),c)
+            temp += combi
+            
+        counter = Counter(temp)
+        print(counter)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            for f in counter:
+                if counter[f] == max(counter.values()):
+                    answer += [''.join(f)]
+                    print(answer)
+    return sorted(answer)

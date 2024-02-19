75d6949f9ef495770943451afe1ce2b57248e4ac
jihhyeon <sjh1031201@naver.com>
2024-02-19 12:05:49 +0900

    save

--- 구현/14719.py
+++ 구현/14719.py
@@ -1,14 +1,14 @@
 h, w = map(int, input().split())
-block = [map(int, input().split())]
-rain = 0
+world = list(map(int, input().split()))
 
-for i in range(1, w-1):
-    left_max = max(block[:i])
-    right_max = max(block[i+1:])
+ans = 0
+for i in range(1, w - 1):
+    left_max = max(world[:i])
+    right_max = max(world[i+1:])
 
     compare = min(left_max, right_max)
 
-    if block[i] < compare:
-        rain += compare - block[i]
+    if world[i] < compare:
+        ans += compare - world[i]
 
-print(rain)
+print(ans)
